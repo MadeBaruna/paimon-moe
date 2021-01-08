5badfa804dbb58483b5ab52e5e71d76225e8c77c
I Made Setia Baruna <setia.baruna@sprintasia.co.id>
2021-01-09 01:27:08 +0800

    Add netlify toml

---
+++ scripts/createEnv.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+let envString = '';
+envString += `GOOGLE_DRIVE_CLIENT_ID=${process.env.GOOGLE_DRIVE_CLIENT_ID}\n`;
+envString += `GOOGLE_DRIVE_API_KEY=${process.env.GOOGLE_DRIVE_API_KEY}\n`;
+
+fs.writeFileSync('.env', envString);
