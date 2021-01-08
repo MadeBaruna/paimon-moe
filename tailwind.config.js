a714eeabe451e1fe4541b8e0d2583d516fc4743e
I Made Setia Baruna <setia.baruna@sprintasia.co.id>
2021-01-09 04:44:51 +0800

    Fix purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    mode: 'all',
     content: ['./**/**/*.html', './**/**/*.svelte'],
     options: {
       whitelistPatterns: [/svelte-/],
