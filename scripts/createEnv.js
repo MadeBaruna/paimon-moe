fd9609914015537ab672084e6e122ab1525ec833
Made Baruna <made.setia@gmail.com>
2021-03-29 05:43:16 +0800

    Fix env

--- scripts/createEnv.js
+++ scripts/createEnv.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 let envString = '';
 envString += `GOOGLE_DRIVE_CLIENT_ID=${process.env.GOOGLE_DRIVE_CLIENT_ID}\n`;
 envString += `GOOGLE_DRIVE_API_KEY=${process.env.GOOGLE_DRIVE_API_KEY}\n`;
-envString += `CORS_HOST=${process.env.CORS_HOST}\n`;
+envString += `API_HOST=${process.env.API_HOST}\n`;
 
 fs.writeFileSync('.env', envString);
