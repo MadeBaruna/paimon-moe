ec1a3dc24b51745811eb9e2942755b0a27b9ac3b
Made Baruna <made.setia@gmail.com>
2021-03-31 05:18:52 +0800

    Update conflict modal wording - close #20

--- src/components/SyncConflictModal.svelte
+++ src/components/SyncConflictModal.svelte
@@ -1,7 +1,8 @@
 <script>
   import { mdiCloudAlert, mdiContentSave, mdiDownload, mdiFile, mdiGoogleDrive, mdiLoading, mdiUpload } from '@mdi/js';
+  import { t } from 'svelte-i18n';
+  
   import Button from './Button.svelte';
-
   import Icon from './Icon.svelte';
 
   export let remoteTime;
@@ -30,34 +31,34 @@
 <div class="flex">
   <Icon path={mdiCloudAlert} color="white" size={2} />
   <p class="flex-1 text-white text-lg md:text-xl font-bold ml-4">
-    Your local data is conflicting with the ones stored in the Google Drive!
+    {$t('sync.message')}
   </p>
 </div>
 <div class="mt-4 p-4 bg-item rounded-xl text-white">
   <p class="font-bold">
     <Icon path={mdiGoogleDrive} className="mr-2" />
-    Google Drive Data -
-    {remoteNewer ? 'NEWER' : 'OLDER'}
+    {$t('sync.googleDriveData')} -
+    <span class={remoteNewer ? 'text-green-400' : 'text-red-400'}>{remoteNewer ? $t('sync.newer') : $t('sync.older')}</span>
   </p>
-  <p class="text-gray-400 mt-1">Last modified: {remoteFormatted}</p>
+  <p class="text-gray-400 mt-1">{$t('sync.lastModified')}: {remoteFormatted}</p>
   <Button disabled={loading} className="mt-2 w-full" on:click={useRemoteData}>
-    <Icon path={loading ? mdiLoading : mdiDownload} spin={loading} className="mr-1" />Replace Local Data
+    <Icon path={loading ? mdiLoading : mdiDownload} spin={loading} className="mr-1" />{$t('sync.useGoogleDriveData')}
   </Button>
 </div>
-<p class="mt-2 text-white text-center">OR</p>
+<p class="mt-2 text-white text-center">{$t('sync.or')}</p>
 <div class="mt-2 p-4 bg-item rounded-xl text-white">
   <p class="font-bold">
     <Icon path={mdiFile} className="mr-2" />
-    Local Data -
-    {remoteNewer ? 'OLDER' : 'NEWER'}
+    {$t('sync.localData')} -
+    <span class={remoteNewer ? 'text-red-400' : 'text-green-400'}>{remoteNewer ? $t('sync.older') : $t('sync.newer')}
   </p>
-  <p class="text-gray-400 mt-1">Last modified: {localFormatted}</p>
+  <p class="text-gray-400 mt-1">{$t('sync.lastModified')}: {localFormatted}</p>
   <Button disabled={loading} className="mt-2 w-full" on:click={useLocalData}>
-    <Icon path={loading ? mdiLoading : mdiUpload} spin={loading} className="mr-1" />Replace Google Drive Data
+    <Icon path={loading ? mdiLoading : mdiUpload} spin={loading} className="mr-1" />{$t('sync.useLocalData')}
   </Button>
 </div>
 <div class="flex mt-6 justify-end">
   <Button className="w-full md:w-auto" on:click={downloadBackup}>
-    <Icon path={mdiContentSave} className="mr-1" />Download Both Data
+    <Icon path={mdiContentSave} className="mr-1" />{$t('sync.download')}
   </Button>
 </div>
