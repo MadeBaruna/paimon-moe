a74e3704d9c4a7f4b9de631b5201c0b2d48f7c8b
Made Baruna <made.setia@gmail.com>
2021-04-01 17:36:57 +0800

    Update privacy policy

--- src/routes/privacy-policy.svelte
+++ src/routes/privacy-policy.svelte
@@ -7,14 +7,40 @@
   <div class="px-4 md:px-8 text-white">
     <h3 class="text-xl font-bold">{$t('privacypolicy.subtitle')}</h3>
     <ul class="list-disc ml-4">
-      {#each new Array(3) as _, i}
-        <li class="mt-2">
-          <span class="font-bold">{$t(`privacypolicy.collect.${i}.title`)}</span><br/>
-          {$t(`privacypolicy.collect.${i}.content`)}
-        </li>
-      {/each}
+      <li class="mt-2">
+        <span class="font-bold">{$t(`privacypolicy.collect.0.title`)}</span><br />
+        {$t(`privacypolicy.collect.0.content.0`)}
+        <a target="_blank" class="text-primary hover:underline" href="https://plausible.io">
+          {$t(`privacypolicy.collect.0.content.1`)}
+        </a>
+        {$t(`privacypolicy.collect.0.content.2`)}
+      </li>
+      <li class="mt-2">
+        <span class="font-bold">{$t(`privacypolicy.collect.1.title`)}</span><br />
+        {$t(`privacypolicy.collect.1.content.0`)}
+        <a target="_blank" class="text-primary hover:underline" href="https://sentry.io">
+          {$t(`privacypolicy.collect.1.content.1`)}
+        </a>
+        {$t(`privacypolicy.collect.1.content.2`)}
+      </li>
+      <li class="mt-2">
+        <span class="font-bold">{$t(`privacypolicy.collect.2.title`)}</span><br />
+        {$t(`privacypolicy.collect.2.content.0`)}
+        <a target="_blank" class="text-primary hover:underline" href="/wish/tally">
+          {$t(`privacypolicy.collect.2.content.1`)}
+        </a>
+      </li>
     </ul>
     <h3 class="text-xl font-bold mt-4">{$t('privacypolicy.subtitleNotCollect')}</h3>
-    <p>{$t('privacypolicy.notCollect')}</p>
+    <p>
+      {$t('privacypolicy.notCollect.0')}
+      <a target="_blank" class="text-primary hover:underline" href="https://github.com/MadeBaruna/paimon-moe-api">
+        {$t(`privacypolicy.notCollect.1`)}
+      </a>
+      {$t('privacypolicy.notCollect.2')}
+      <a target="_blank" class="text-primary hover:underline" href="https://github.com/MadeBaruna/paimon-moe">
+        {$t(`privacypolicy.notCollect.3`)}
+      </a>
+    </p>
   </div>
 </div>
