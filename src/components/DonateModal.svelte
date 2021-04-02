b7f2084f3d31fd49125dd8ca0ef5ad136fcaec6b
Made Baruna <made.setia@gmail.com>
2021-04-02 16:33:42 +0800

    Localize donate

--- src/components/DonateModal.svelte
+++ src/components/DonateModal.svelte
@@ -1,19 +1,15 @@
 <script>
+import { t } from 'svelte-i18n';
+
   import Button from './Button.svelte';
 </script>
 
 <div class="text-white ">
-  <h1 class="font-semibold">Thank you! Hope you find it useful.</h1>
-  <p class="mt-2">
-    I made this website for fun when I have some spare time. Obviously will add more feature and update the data every
-    update, because I like to explore the game and kind of min-maxing it.
-  </p>
+  <h1 class="font-semibold">{$t('donate.message.0')}</h1>
+  <p class="mt-2">{$t('donate.message.1')}</p>
+  <p class="mt-1">{$t('donate.message.2')}</p>
   <p class="mt-1">
-    If you want to donate, your donation will definitely help on improving the site and motivating me to add more stuff
-    to Paimon.moe! Thank you very much and have fun, hope you like it!
-  </p>
-  <p class="mt-1">
-    You can also contributing to the project on <a
+    {$t('donate.message.3')} <a
       target="_blank"
       class="text-primary hover:underline"
       href="https://github.com/MadeBaruna/paimon-moe"
@@ -27,7 +23,7 @@
           <img class="h-6 mr-1" src="/images/donation/ko-fi.png" alt="Ko-fi" />
           <img class="h-6" src="/images/donation/paypal.png" alt="PayPal" />
         </div>
-        <span class="font-semibold">Support me on Ko-fi</span>
+        <span class="font-semibold">{$t('donate.kofi')}</span>
       </Button>
     </a>
     <a class="flex-1 md:ml-1" href="https://trakteer.id/baruna" target="_blank">
@@ -38,7 +34,7 @@
           <img class="h-4 mr-1" src="/images/donation/ovo.png" alt="gopay" />
           <img class="h-4" src="/images/donation/dana.png" alt="gopay" />
         </div>
-        <span class="font-semibold">Support me on Trakteer</span>
+        <span class="font-semibold">{$t('donate.trakteer')}</span>
       </Button>
     </a>
   </div>
