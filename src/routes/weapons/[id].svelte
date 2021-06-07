ae8737aaab78e1c8e1bf59b197a43eb70dfd4934
Made Baruna <made.setia@gmail.com>
2021-06-07 13:27:32 +0700

    Add weapons page

---
+++ src/routes/weapons/[id].svelte
@@ -0,0 +1,111 @@
+<script context="module">
+  import data from '../../data/weapons/en.json';
+  import { weaponList } from '../../data/weaponList.js';
+  export async function preload(page) {
+    const { id } = page.params;
+    const weapon = data[id];
+    const materials = weaponList[id].ascension[0].items;
+
+    return { id, weapon, materials };
+  }
+
+</script>
+
+<script>
+  import { mdiCircle, mdiStar } from '@mdi/js';
+  import { t } from 'svelte-i18n';
+  import Icon from '../../components/Icon.svelte';
+
+  const rarity = {
+    1: 'text-white',
+    2: 'text-green-400',
+    3: 'text-primary',
+    4: 'text-rare-from',
+    5: 'text-legendary-from',
+  };
+
+  /* prettier-ignore */
+  const showedIndex = [1, 6, 11, 16, 20, 21, 26, 31, 36, 41, 42, 47, 52, 53, 58, 63, 64, 69, 74, 75, 80, 85, 86, 91, 96];
+  const level = [1, 5, 10, 15, 20, 20, 25, 30, 35, 40, 40, 45, 50, 50, 55, 60, 60, 65, 70, 70, 75, 80, 80, 85, 90];
+  const ascen = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6];
+  const ascenSpan = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+
+  export let id;
+  export let weapon;
+  // export let materials;
+
+  $: multiplier = weapon.secondary.name === 'em' ? 1 : 100;
+  $: suffix = weapon.secondary.name === 'em' ? '' : '%';
+
+</script>
+
+<svelte:head>
+  <title>{weapon.name} - Paimon.moe</title>
+</svelte:head>
+<div class="lg:ml-64 pt-20 lg:pt-8 max-w-screen-xl md:pl-4">
+  <div class="flex flex-col-reverse xl:flex-row items-start">
+    <div class="px-4">
+      <h1 class="font-display font-black text-4xl md:text-5xl text-white">{weapon.name}</h1>
+      <div class="{rarity[weapon.rarity]} text-2xl flex items-center z-0 -mt-2 md:-mt-4">
+        {#each [...new Array(weapon.rarity)] as _}
+          <Icon path={mdiStar} />
+        {/each}
+        <Icon path={mdiCircle} size={0.4} className="mx-2 mt-1" color="white" />
+        <p class="text-base text-white font-semibold mt-1">{$t(`weapon.${weapon.type}`)}</p>
+      </div>
+      <p class="text-gray-200">{weapon.description}</p>
+      {#if weapon.skill.name}
+        <div class="p-4 rounded-xl bg-item flex flex-col mt-4">
+          <p class="font-black font-display text-xl text-white">{weapon.skill.name}</p>
+          <p class="skill-description text-white">{@html weapon.skill.description}</p>
+        </div>
+      {/if}
+      <div class="mt-4 block overflow-x-auto whitespace-no-wrap md:w-auto">
+        <div style="width: min-content;">
+          <div class="table max-w-full rounded-xl border border-gray-200 border-opacity-25">
+            <table class="text-gray-200 w-full">
+              <tr>
+                <td class="text-center whitespace-no-wrap border-gray-700 font-semibold px-2">
+                  {$t('weapon.asc')}
+                </td>
+                <td class="text-center whitespace-no-wrap border-gray-700 font-semibold px-2">
+                  {$t('weapon.lvl')}
+                </td>
+                <td class="text-center whitespace-no-wrap border-gray-700 font-semibold px-2">
+                  {$t('weapon.baseAtk')}
+                </td>
+                {#if weapon.secondary.name}
+                  <td class="text-center whitespace-no-wrap border-gray-700 font-semibold px-2">
+                    {$t(`weapon.${weapon.secondary.name}`)}
+                  </td>
+                {/if}
+              </tr>
+              {#each showedIndex as index, i}
+                <tr>
+                  {#if [0, 5, 10, 13, 16, 19, 22].includes(i)}
+                    <td rowspan={ascenSpan[i]} class="text-center border-t border-gray-700 px-2">{ascen[i]}</td>
+                  {/if}
+                  <td class="text-center border-t border-gray-700 px-2">{level[i]}</td>
+                  <td class="text-center border-t border-gray-700 px-2">{Math.round(weapon.atk[index])}</td>
+                  {#if weapon.secondary.stats}
+                    <td class="text-center border-t border-gray-700 px-2">
+                      {Math.round(weapon.secondary.stats[index] * multiplier)}{suffix}
+                    </td>
+                  {/if}
+                </tr>
+              {/each}
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <img class="w-64 h-64 ml-4 max-w-full" src="/images/weapons/{id}.png" alt={weapon.name} />
+  </div>
+</div>
+
+<style>
+  td:not(:last-child) {
+    @apply border-r;
+  }
+
+</style>
---
+++ src/routes/weapons/index.svelte
@@ -0,0 +1,152 @@
+<script context="module">
+  import data from '../../data/weapons/en.json';
+  export async function preload() {
+    return { data };
+  }
+
+</script>
+
+<script>
+  import { t } from 'svelte-i18n';
+  import TableHeader from '../../components/Table/TableHeader.svelte';
+
+  export let data;
+  let weaponList = [];
+  let sortBy = 'name';
+  let sortOrder = true;
+
+  const rarity = {
+    2: 'text-green-400',
+    3: 'text-primary',
+    4: 'text-rare-from',
+    5: 'text-legendary-from',
+  };
+
+  function sort(by) {
+    if (sortBy === by) {
+      sortOrder = !sortOrder;
+    } else {
+      sortBy = by;
+    }
+  }
+
+  function process() {
+    const _weapons = [];
+    for (const [id, weapon] of Object.entries(data)) {
+      if (['amber_bead', 'ebony_bow', 'quartz', 'the_flagstaff'].includes(id)) continue;
+
+      _weapons.push({
+        id,
+        name: weapon.name,
+        type: weapon.type,
+        rarity: weapon.rarity,
+        atk: weapon.atk[weapon.atk.length - 1].toFixed(0),
+        secondary: weapon.secondary.name
+          ? `${$t(`weapon.${weapon.secondary.name}`)} ${(
+              weapon.secondary.stats[weapon.secondary.stats.length - 1] * (weapon.secondary.name === 'em' ? 1 : 100)
+            ).toFixed(0)}${weapon.secondary.name === 'em' ? '' : '%'}`
+          : '-',
+      });
+    }
+
+    weaponList = _weapons;
+  }
+
+  process();
+
+  $: weapons = weaponList.sort((a, b) => {
+    switch (sortBy) {
+      case 'name':
+        if (sortOrder) {
+          return a.name.localeCompare(b.name);
+        } else {
+          return b.name.localeCompare(a.name);
+        }
+      case 'type':
+        if (sortOrder) {
+          return a.type.localeCompare(b.type);
+        } else {
+          return b.type.localeCompare(a.type);
+        }
+      case 'rarity':
+        if (sortOrder) {
+          return a.rarity - b.rarity;
+        } else {
+          return b.rarity - a.rarity;
+        }
+      case 'atk':
+        if (sortOrder) {
+          return a.atk - b.atk;
+        } else {
+          return b.atk - a.atk;
+        }
+      case 'secondary':
+        if (sortOrder) {
+          return a.secondary.localeCompare(b.secondary);
+        } else {
+          return b.secondary.localeCompare(a.secondary);
+        }
+    }
+  });
+
+</script>
+
+<svelte:head>
+  <title>Weapon List - Paimon.moe</title>
+  <meta name="description" content="Genshin Impact Weapon list and stats" />
+  <meta property="og:description" content="Genshin Impact Weapon list and stats" />
+</svelte:head>
+<div class="lg:ml-64 pt-20 lg:pt-8">
+  <h1 class="font-display px-4 md:px-8 font-black text-5xl text-white">{$t('weapon.title')}</h1>
+  <p class="text-gray-400 px-4 md:px-8 font-medium pb-4" style="margin-top: -1rem;">
+    ※ {$t('weapon.subtitle')}
+  </p>
+  <div class="block overflow-x-auto whitespace-no-wrap pb-8">
+    <div class="px-4 md:px-8 table max-w-full">
+      <table class="w-full block p-4 bg-item rounded-xl">
+        <thead>
+          <TableHeader />
+          <TableHeader on:click={() => sort('name')} sort={sortBy === 'name'} order={sortOrder}>
+            {$t('weapon.name')}
+          </TableHeader>
+          <TableHeader on:click={() => sort('type')} sort={sortBy === 'type'} order={sortOrder}>
+            {$t('weapon.type')}
+          </TableHeader>
+          <TableHeader on:click={() => sort('rarity')} sort={sortBy === 'rarity'} order={sortOrder}>
+            {$t('weapon.rarity')}
+          </TableHeader>
+          <TableHeader on:click={() => sort('atk')} sort={sortBy === 'atk'} order={sortOrder}>
+            {$t('weapon.atk')}
+          </TableHeader>
+          <TableHeader on:click={() => sort('secondary')} sort={sortBy === 'secondary'} order={sortOrder}>
+            {$t('weapon.secondary')}
+          </TableHeader>
+        </thead>
+        <tbody class="text-white">
+          {#each weapons as weapon (weapon.id)}
+            <a href="/weapons/{weapon.id}" style="display: contents;">
+              <tr class="cursor-pointer hover:bg-background">
+                <td class="border-gray-700 border-t py-1 text-center h-12">
+                  <img src="/images/weapons/{weapon.id}.png" alt={weapon.type} class="h-full w-auto inline" />
+                </td>
+                <td class="border-gray-700 border-t py-1 pl-4 pr-2">
+                  {weapon.name}
+                </td>
+                <td class="border-gray-700 border-t py-1 text-center h-8">
+                  <img src="/images/weapons/{weapon.type}.png" alt={weapon.type} class="h-full w-auto inline" />
+                </td>
+                <td class="border-gray-700 border-t py-1 text-center text-2xl {rarity[weapon.rarity]}"> ★ </td>
+                <td class="border-gray-700 border-t py-1 px-1 text-center">
+                  {weapon.atk}
+                </td>
+                <td class="border-gray-700 border-t py-1 pl-4">
+                  {weapon.secondary}
+                </td>
+              </tr>
+            </a>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
