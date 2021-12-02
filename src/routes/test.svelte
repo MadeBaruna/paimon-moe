6fb9f7c6ddff4e66b84395feaa01eb35cdcbc92d
Made Baruna <made.setia@gmail.com>
2021-12-03 00:13:36 +0700

    Test page

---
+++ src/routes/test.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Ad from '../components/Ad.svelte';
+</script>
+
+<div class="pt-20 lg:ml-64 lg:pt-8 px-4 md:px-8 max-w-screen-lg">
+  <Ad type="desktop" variant="player" id="" style="width: 100%; height: 500px;" />
+</div>
