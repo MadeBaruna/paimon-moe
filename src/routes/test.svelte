5e94e11c3efb4b46726e68fb33a7851ceab00cda
Made Baruna <made.setia@gmail.com>
2021-12-03 20:05:17 +0700

    Update test page

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <div class="pt-20 lg:ml-64 lg:pt-8 px-4 md:px-8 max-w-screen-lg">
+  <Ad type="desktop" variant="lb" id="1" class="mb-2" />
   <Ad type="desktop" variant="player" id="" style="width: 100%; height: 500px;" />
 </div>
