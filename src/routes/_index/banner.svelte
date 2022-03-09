05dbce589eb1ee70655dda6feca6b1d48018f045
Made Baruna <made.setia@gmail.com>
2022-03-09 23:27:04 +0700

    Fix homepage banner number on mobile device

--- src/routes/_index/banner.svelte
+++ src/routes/_index/banner.svelte
@@ -74,12 +74,12 @@
 <div class="bg-item rounded-xl p-4 flex flex-col">
   <div class="relative">
     <img src="/images/home/{image}" alt="banner" style="min-height: 150px;" />
-    <div class="flex flex-wrap text-white justify-between items-center absolute bottom-0 w-full">
+    <div class="flex flex-wrap text-white justify-end items-center absolute bottom-0 w-full">
       {#each Object.entries(featured) as [_, item], i}
         <div
           class="flex flex-col pt-2"
           style="background: linear-gradient(270deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.75) 17%, rgba(0,0,0,0.75) 70%, rgba(0,0,0,0) 100%); 
-          {i === 0 ? 'margin-right: 10%; margin-left: 30%;' : ''}"
+          {i === 0 ? 'margin-right: 10%;' : ''}"
         >
           <h3 class="text-3xl mr-4 font-black leading-6 text-right">
             {#if loading}
