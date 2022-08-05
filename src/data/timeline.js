87de949fe82c51289b3a27ba5f87845851468f38
Made Baruna <made.setia@gmail.com>
2022-08-05 10:08:44 +0700

    Fix timeline link

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -149,7 +149,7 @@ export const eventsData = [
       end: '2022-08-22 03:59:59',
       color: '#ffc284',
       zoom: '100%',
-      url: 'https://www.hoyolab.com/article/617391',
+      url: 'https://www.hoyolab.com/article/6197391',
       showOnHome: true,
     },
   ],
