5b60b018c585a11f23022744eaef205bfe54fb39
Made Baruna <made.setia@gmail.com>
2022-09-20 13:59:26 +0700

    Adjust banner number on frontpage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   export async function load({ fetch }) {
-    const promoted = ['xingqiu', 'sucrose'];
+    const promoted = ['ganyu', 'sangonomiya_kokomi'];
     const builds = {};
     for (const p of promoted) {
       const response = await fetch(`/characters/build/${p}.json`);
