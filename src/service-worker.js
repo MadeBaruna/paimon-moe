2b23f668ceabe884bb93da840933ad180909a665
Made Baruna <made.setia@gmail.com>
2024-08-28 15:18:19 +0800

    Update changelog

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,7 +7,7 @@ const IMAGE_CACHE = `cacheimg${IMAGE_CACHE_VER}`;
 
 const IMAGE_URL = `${self.location.origin}/images/`;
 
-const changelog = ['Update banners', 'Update timeline'];
+const changelog = ['Update banners', 'Update characters & weapons', 'Update achievements', 'Update timeline'];
 
 const channel = new BroadcastChannel('paimonmoe-sw');
 
